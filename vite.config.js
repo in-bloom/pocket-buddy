744db6f4ed68c5fb305c19429d93d9b77d00316a
in-bloom <f.ricciardi9@studenti.unipi.it>
2024-09-27 14:04:40 +0200

    made the pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,46 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 /* import { VitePWA } from "vite-plugin-pwa"; */
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        name: "Pocket BUddy",
+        short_name: "PocketBuddy",
+        description: "Gestisci le tue spese in modo semplice",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "mask-icon.svg",
+            sizes: "512x512",
+            type: "image/svg+xml",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
 });
