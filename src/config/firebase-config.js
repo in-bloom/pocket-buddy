320ea2f88e2136f1676a1925b4583b1cde51aa60
in-bloom <f.ricciardi9@studenti.unipi.it>
2024-10-03 17:56:24 +0200

    speriamo che funzioni ora

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -10,26 +10,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey:
-    process.env.REACT_APP_FIREBASE_API_KEY ||
-    functions.config().firebase.api_key,
-  authDomain:
-    process.env.REACT_APP_FIREBASE_AUTH_DOMAIN ||
-    functions.config().firebase.auth_domain,
-  projectId:
-    process.env.REACT_APP_FIREBASE_PROJECT_ID ||
-    functions.config().firebase.project_id,
-  storageBucket:
-    process.env.REACT_APP_FIREBASE_STORAGE_BUCKET ||
-    functions.config().firebase.storage_bucket,
-  messagingSenderId:
-    process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID ||
-    functions.config().firebase.messaging_sender_id,
-  appId:
-    process.env.REACT_APP_FIREBASE_APP_ID || functions.config().firebase.app_id,
-  measurementId:
-    process.env.REACT_APP_FIREBASE_MEASUREMENT_ID ||
-    functions.config().firebase.measurement_id,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
