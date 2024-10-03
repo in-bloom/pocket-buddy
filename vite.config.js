86e1db7e448c6d21546ba236d33f1225d8a362a8
in-bloom <f.ricciardi9@studenti.unipi.it>
2024-10-03 23:36:12 +0200

    sos

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
+import dotenv from 'dotenv';
+dotenv.config();
 /* import { VitePWA } from "vite-plugin-pwa"; */
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    "process.env": process.env,
+  },
   plugins: [
     react(),
     VitePWA({
